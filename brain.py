c34c15b8ed904abbdbf94330258e4a3237a24394
Imaad Uddin <imaaduddin@icloud.com>
2021-06-29 22:47:13 -0500

    attempt

--- brain.py
+++ brain.py
@@ -1,1 +1,7 @@
 from imageai.Prediction  import ImagePrediction
+import os 
+execution_path = os.getcwd()
+
+prediction = ImagePrediction()
+prediction.setModelTypeAsSqueezeNet()
+prediction.setModelPath(os.path.join(execution_path, ""))
