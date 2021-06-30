083ff182c7a327e2893e6666b8069fe127b91e80
Imaad Uddin <imaaduddin@icloud.com>
2021-06-29 22:59:59 -0500

    final

--- brain.py
+++ brain.py
@@ -3,5 +3,9 @@ import os
 execution_path = os.getcwd()
 
 prediction = ImagePrediction()
-prediction.setModelTypeAsSqueezeNet()
-prediction.setModelPath(os.path.join(execution_path, ""))
+prediction.setModelTypeAsResNet()
+prediction.setModelPath(os.path.join(execution_path, "resnet50_weights_tf_dim_ordering_tf_kernels.h5"))
+
+predictions, probabilities = prediction.predictImage(os.path.join(execution_path, ""), result_count=5)
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+  print(eachPrediction, " : ", eachProbability)
